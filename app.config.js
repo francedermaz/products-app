324e50cd31bced98ef34ceabf571244c8dc590bf
Francisco Cedermaz <francedermaz@gmail.com>
2025-06-07 17:17:32 -0300

    feat: updates to make deeplink work cleanup

--- app.config.js
+++ app.config.js
@@ -1,11 +1,36 @@
-import 'dotenv/config';
+import "dotenv/config";
 
-export default {
-  expo: {
-    name: "products-app",
-    slug: "products-app",
-    extra: {
-      apiUrl: process.env.API_URL,
-    },
+export default ({ config }) => ({
+  ...config,
+  name: "products-app",
+  slug: "products-app",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  userInterfaceStyle: "light",
+  newArchEnabled: true,
+  splash: {
+    image: "./assets/splash-icon.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
   },
-};
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: "com.fran.products",
+  },
+  android: {
+    package: "com.fran.products",
+    adaptiveIcon: {
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#ffffff",
+    },
+    edgeToEdgeEnabled: true,
+  },
+  web: {
+    favicon: "./assets/favicon.png",
+  },
+  scheme: "products",
+  extra: {
+    apiUrl: process.env.API_URL,
+  },
+});
